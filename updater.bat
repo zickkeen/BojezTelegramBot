81e6e7c7102becc35c3b28a98d519c5eb0f35e66
zickkeen <jackkinny@gmail.com>
2021-01-05 21:43:35 +0700

    new updater info and readme fix

--- updater.bat
+++ updater.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem -------------------------------------------------------------
-rem  Yii command line bootstrap script for Windows.
+rem  Windows Updater "longman php telegram bot".
 rem
-rem  @author Qiang Xue <qiang.xue@gmail.com>
-rem  @link http://www.yiiframework.com/
-rem  @copyright Copyright (c) 2008 Yii Software LLC
-rem  @license http://www.yiiframework.com/license/
+rem  @zickkeen <zickkeen@gmail.com>
+rem  @link http://bojez.com/
+rem  @copyright Copyright (c) 2020 Bojez Creative
+rem  @license MIT License
 rem -------------------------------------------------------------
 
 @setlocal
